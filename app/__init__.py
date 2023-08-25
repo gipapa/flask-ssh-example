5e3fc71b3dd0aa6886c4561c8316471bd92d4392
gipapa <gipapa.cs94@g2.nctu.edu.tw>
2023-08-25 11:45:34 +0800

    fix requirement

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 from . import routes  # NOQA
 
 app.config['SSH_USERNAME'] = "root"
-app.config['SSH_HOSTNAME'] = "example.com"
+app.config['SSH_HOSTNAME'] = "web-p174.onrender.com"
 app.config['SSH_PRIV_KEY'] = """
 -----BEGIN RSA PRIVATE KEY-----
-...
+root5566
 -----END RSA PRIVATE KEY-----
 """
